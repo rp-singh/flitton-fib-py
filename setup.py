22481efec54674ba6f628ed9d8144456c6ae62b2
rp <rupinderpalsingh@gmail.com>
2023-05-25 23:45:39 +1000

    command line interface

--- setup.py
+++ setup.py
@@ -20,4 +20,9 @@ setup(
     ],
     python_requires='>=3',
     tests_require=['pytest']
+    entry_points={
+        'console_scripts': [
+            'fib-number = flitton_fib_py.cmd.fib_numb:fib_numb'
+        ]
+    }
 )
