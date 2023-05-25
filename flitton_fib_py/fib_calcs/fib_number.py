9a901cfc865e3f38c6b9bb9034946714736e6e4d
rp <rupinderpalsingh@gmail.com>
2023-05-25 22:40:29 +1000

    resove issue

--- flitton_fib_py/fib_calcs/fib_number.py
+++ flitton_fib_py/fib_calcs/fib_number.py
@@ -1,4 +1,6 @@
-def recurring_fibonacci_number(number: int) -> int | None:
+from typing import Optional
+
+def recurring_fibonacci_number(number: int) -> Optional[int]:
     if number < 0:
         return None
     elif number <= 1:
